c2e0d468ee63d9f24347b5f26c48d335eb224139
HuynhTinh <tinh_2251220008@dau.edu.vn>
2026-04-21 18:48:54 +0700

    Cập nhật: Xử lý bộ lọc sản phẩm tự động áp dụng khi click, hoàn thiện tự động chốt đơn Chatbot và sửa UI Thông tin cá nhân

--- users/urls.py
+++ users/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('verify-otp/', views.verify_otp_view, name='verify_otp'),
     path('reset-password/', views.reset_password_view, name='reset_password'),
+    
+    # Tính năng cập nhật thông tin cá nhân
+    path('profile/', views.profile_view, name='profile'),
 ]
