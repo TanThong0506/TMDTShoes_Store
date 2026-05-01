f58f031db74ace4626fc10b9ea62b98377e87b66
TanThong <thong_2251220034@dau.edu.vn>
2026-05-02 03:57:44 +0700

    Add merge migration for orders

---
+++ orders/migrations/0007_merge_0006_alter_order_options_0006_order_return_reason_alter_order_status.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0006_alter_order_options'),
+        ('orders', '0006_order_return_reason_alter_order_status'),
+    ]
+
+    operations = []
